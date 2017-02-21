d26ed3f147de709002d6e3535891662772a10649
isabel <024631@daltonk12.onmicrosoft.com>
2017-02-21 10:55:11 -0500

    Update team3.py

--- team3.py
+++ team3.py
@@ -6,27 +6,30 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'Octahedron' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Octahedron'
+strategy_name = 'SIKE!!!'
+strategy_description = '''\
+Collude first round. Collude, except in a round after getting 
+a severe punishment.'''
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
+    '''Make my move based on the history with this player.
     
-    Make my move.
-    Returns 'c' or 'b'. 
+    history: a string with one letter (c or b) per round that has been played with this opponent.
+    their_history: a string of the same length as history, possibly empty. 
+    The first round between these two players is my_history[0] and their_history[0]
+    The most recent round is my_history[-1] and their_history[-1]
+    
+    Returns 'c' or 'b' for collude or betray.
     '''
+    if len(my_history)==0: # It's the first round; collude.
+        return 'c'
+    elif my_history[-1]=='c' and their_history[-1]=='b':
+        return 'b' # Betray if they were severely punished last time,
+    else:
+        return 'c' # otherwise collude.
+    
 
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
@@ -66,3 +69,6 @@ if __name__ == '__main__':
               my_score=0, 
               their_score=0,
               result='b')             
+
+
+    
