02e845faf189f8cd1a9761490279c9f2fe7ccbc4
LuisArias24 <025152@daltonk12.onmicrosoft.com>
2017-02-21 14:34:31 -0500

    strategie
    copy and paste strategie into team file

--- team7.py
+++ team7.py
@@ -6,17 +6,23 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'Daijoubous' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'daijobous'
+strategy_name = 'Collude'
+strategy_description = 'Always collude.'
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
+    '''Make my move based on the history with this player.
     
-    Make my move.
-    Returns 'c' or 'b'. 
+    history: a string with one letter (c or b) per round that has been played with this opponent.
+    their_history: a string of the same length as history, possibly empty. 
+    The first round between these two players is my_history[0] and their_history[0]
+    The most recent round is my_history[-1] and their_history[-1]
+    
+    Returns 'c' or 'b' for collude or betray.
     '''
+    
+    # This player always colludes.
+    return 'c'
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
